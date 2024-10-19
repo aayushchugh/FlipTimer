89d16050b7e361ff9b22d951af44b005d56b6f42
Ayush Chugh <hey@ayushchugh.com>
2024-10-19 23:45:35 +0530

    feat: times up

--- components/FlipClock.tsx
+++ components/FlipClock.tsx
@@ -7,6 +7,7 @@ interface IFlipClockProps {
 }
 
 const FlipClock: FC<IFlipClockProps> = ({ targetDate }) => {
+	const [isTimesUp, setIsTimesUp] = useState(false);
 	const [timeLeft, setTimeLeft] = useState({
 		months: 0,
 		weeks: 0,
@@ -21,6 +22,7 @@ const FlipClock: FC<IFlipClockProps> = ({ targetDate }) => {
 		const difference = +new Date(targetDate) - +new Date();
 
 		if (difference > 0) {
+			setIsTimesUp(false);
 			setTimeLeft({
 				months: Math.floor(difference / (1000 * 60 * 60 * 24 * 30)), // 30 days
 				weeks: Math.floor(difference / (1000 * 60 * 60 * 24 * 7)), // 7 days
@@ -28,6 +30,16 @@ const FlipClock: FC<IFlipClockProps> = ({ targetDate }) => {
 				hours: Math.floor((difference / (1000 * 60 * 60)) % 24), // 60 minutes
 				minutes: Math.floor((difference / 1000 / 60) % 60), // 60 seconds
 				seconds: Math.floor((difference / 1000) % 60), // 1000 milliseconds
+			});
+		} else {
+			setIsTimesUp(true);
+			setTimeLeft({
+				months: 0,
+				weeks: 0,
+				days: 0,
+				hours: 0,
+				minutes: 0,
+				seconds: 0,
 			});
 		}
 	};
@@ -40,33 +52,36 @@ const FlipClock: FC<IFlipClockProps> = ({ targetDate }) => {
 		return () => clearTimeout(timer);
 	});
 	return (
-		<div className="flex items-center gap-1 sm:gap-3 md:gap-5 lg:gap-7">
-			<div className="flex items-center">
-				<FlipCard title="Months" value={timeLeft?.months.toString()} />
-				<Column />
+		<div className="flex flex-col items-center justify-center">
+			<div className="flex items-center gap-1 sm:gap-3 md:gap-5 lg:gap-7">
+				<div className="flex items-center">
+					<FlipCard title="Months" value={timeLeft?.months.toString()} />
+					<Column />
+				</div>
+				<div className="flex items-center">
+					<FlipCard title="Weeks" value={timeLeft?.weeks.toString()} />
+					<Column />
+				</div>
+				<div className="flex items-center">
+					<FlipCard title="Days" value={timeLeft?.days.toString()} />
+					<Column />
+				</div>
+				<div className="flex items-center">
+					<FlipCard title="Hours" value={timeLeft?.hours.toString()} />
+					<Column />
+				</div>
+				<div className="flex items-center">
+					<FlipCard title="Minutes" value={timeLeft?.minutes.toString()} />
+					<Column />
+				</div>
+				<FlipCard title="Seconds" value={timeLeft?.seconds.toString()} />
 			</div>
 
-			<div className="flex items-center">
-				<FlipCard title="Weeks" value={timeLeft?.weeks.toString()} />
-				<Column />
-			</div>
-
-			<div className="flex items-center">
-				<FlipCard title="Days" value={timeLeft?.days.toString()} />
-				<Column />
-			</div>
-
-			<div className="flex items-center">
-				<FlipCard title="Hours" value={timeLeft?.hours.toString()} />
-				<Column />
-			</div>
-
-			<div className="flex items-center">
-				<FlipCard title="Minutes" value={timeLeft?.minutes.toString()} />
-				<Column />
-			</div>
-
-			<FlipCard title="Seconds" value={timeLeft?.seconds.toString()} />
+			{isTimesUp && (
+				<div className="text-3xl sm:text-4xl md:text-5xl lg:text-3xl text-red-500 mt-10 font-bold">
+					Times Up!
+				</div>
+			)}
 		</div>
 	);
 };
